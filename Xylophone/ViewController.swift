8a7af00ded86d39569cf2f867f337b9de0fc79c0
NSPOLE01 <89700736+NSPOLE01@users.noreply.github.com>
2025-06-22 13:00:10 -0400

    linking buttons to one IBAction

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(file: sender.currentTitle!)
+    }
+    
+    func playSound(file: String){
+        let url = Bundle.main.url(forResource: file, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
     
 
 }
